aa3e4c2ecdeae5d4b1dea4363c598c055f51ff35
Egdus <anders524@hotmail.com>
2013-11-20 21:40:10 +0100

    did some work on the risk register

--- simpleshedsimulator/gui/riskregister.py
+++ simpleshedsimulator/gui/riskregister.py
@@ -1,6 +1,6 @@
-
 import os
 import wx
+import wx.lib.mixins.listctrl  as  listmix
 
 import sys
 path = os.path.dirname(os.path.realpath(__file__))
@@ -25,13 +25,21 @@ from core import reg
 
 [wxID_FRAME1EDITITEMS0] = [wx.NewId() for _init_coll_Edit_Items in range(1)]
 
+class EditableListCtrl(wx.ListCtrl, listmix.TextEditMixin):
+
+    def __init__(self, parent, ID=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize, style=0):
+        wx.ListCtrl.__init__(self, parent, ID, pos, size, style)
+        listmix.TextEditMixin.__init__(self)
 
 
 class Frame1(wx.Frame):
+
+    def __init__(self, parent):
+        self._init_ctrls(parent)
+
     def _init_coll_flexGridSizer1_Items(self, parent):
         # generated method, don't edit
 
-        parent.AddWindow(self.button1, 0, border=0, flag=0)
         parent.AddWindow(self.button2, 0, border=0, flag=0)
         parent.AddWindow(self.button3, 0, border=0, flag=0)
         parent.AddWindow(self.button4, 0, border=0, flag=0)
@@ -101,31 +109,25 @@ class Frame1(wx.Frame):
 
     def _init_ctrls(self, prnt):
         # generated method, don't edit
-        wx.Frame.__init__(self, id=wxID_FRAME1, name='', parent=prnt,
+        wx.Frame.__init__(self, id=wxID_FRAME1, parent=prnt,
               pos=wx.Point(335, 158), size=wx.Size(882, 560),
               style=wx.DEFAULT_FRAME_STYLE, title=u'Risk register')
+        
         self._init_utils()
-        self.SetClientSize(wx.Size(882, 560))
+
         self.SetMenuBar(self.menuBar1)
 
-        self.panel1 = wx.Panel(id=wxID_FRAME1PANEL1, name='panel1', parent=self,
-              pos=wx.Point(0, 0), size=wx.Size(882, 535),
-              style=wx.TAB_TRAVERSAL)
+        self.panel1 = wx.Panel(id=wxID_FRAME1PANEL1, name='panel1',
+                               parent=self,pos=wx.Point(0, 0), 
+                               size=wx.Size(882, 535),style=wx.TAB_TRAVERSAL)
 
-        self.button1 = wx.Button(id=wxID_FRAME1BUTTON1, label=u'Edit entry',
-              name='button1', parent=self.panel1, pos=wx.Point(5, 5),
-              size=wx.Size(85, 29), style=0)
-        self.button1.Bind(wx.EVT_BUTTON, self.OnButton1Button,
-              id=wxID_FRAME1BUTTON1)
-
-        self.button2 = wx.Button(id=wxID_FRAME1BUTTON2, label=u'Add entry',
-              name='button2', parent=self.panel1, pos=wx.Point(98, 5),
-              size=wx.Size(85, 29), style=0)
-
-        self.listCtrl1 = wx.ListCtrl(id=wxID_FRAME1LISTCTRL1, name='listCtrl1',
-              parent=self.panel1, pos=wx.Point(5, 44), size=wx.Size(1290, 501),
-              style=wx.LC_REPORT)
+        self.button2 = wx.Button(id=wxID_FRAME1BUTTON2, label=u'Add entry', name='button2', parent=self.panel1)
+        self.button2.Bind(wx.EVT_BUTTON, self.OnMenu1Items1Menu)
+        
+        self.listCtrl1 = EditableListCtrl(self, style=wx.LC_REPORT)
+       
         self._init_coll_listCtrl1_Columns(self.listCtrl1)
+        
         self.listCtrl1.Bind(wx.EVT_LEFT_DCLICK, self.OnListCtrl1LeftDclick)
 
         self.button3 = wx.Button(id=wxID_FRAME1BUTTON3, label=u'Find',
@@ -141,9 +143,6 @@ class Frame1(wx.Frame):
         self.SetStatusBar(self.statusBar1)
 
         self._init_sizers()
-
-    def __init__(self, parent):
-        self._init_ctrls(parent)
 
     def OnButton3Button(self, event):
             """ Open a file"""
@@ -170,16 +169,7 @@ class Frame1(wx.Frame):
     
     def OnMenu1Items1Menu(self, event):
         #this function creates a dialog which allows you to register a new risk register
-        dlg = Dialog1.Dialog1(self)
-        try:
-            dlg.ShowModal()
-            risk_entry=dlg.new_entry
-        
-            self.listCtrl1.InsertStringItem(0,risk_entry[0])
-            for q in range(0,len(risk_entry)):
-                        self.listCtrl1.SetStringItem(0,q, risk_entry[q])  
-        finally:
-            dlg.Destroy()
+        self.listCtrl1.InsertStringItem(0,"ID")
 
     def OnMenu1Items2Menu(self, event):
         event.Skip()
@@ -188,7 +178,7 @@ class Frame1(wx.Frame):
         #this function deletes the focused entry from memory, but not from the .csv file
         entrynumber=self.listCtrl1.GetFocusedItem()
         data=self.listCtrl1.DeleteItem(entrynumber)
-        return data
+
 
     def OnButton1Button(self, event):
         event.Skip()
