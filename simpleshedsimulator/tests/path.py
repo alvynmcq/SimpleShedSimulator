017080776760c6c2190bf4cd5709f9525d36399f
Egdus <anders524@hotmail.com>
2014-11-10 17:19:47 +0100

    Fixed posix path in path.py module

--- simpleshedsimulator/tests/path.py
+++ simpleshedsimulator/tests/path.py
@@ -1,11 +1,12 @@
 import os
 import sys
 
+print os.name
 if os.name == 'nt':
         path_to_core = "\\".join(sys.path[0].split('\\')[:-1]) + "\\core"
         sys.path.insert(0, path_to_core)
-elif os.name == 'possix':
-        path_to_core = "//".join(sys.path[0].split('//')[:-1]) + "//core"
+elif os.name == 'posix':
+        path_to_core = "/".join(sys.path[0].split('/')[:-1]) + "/core"
         sys.path.insert(0, path_to_core)
 
 
