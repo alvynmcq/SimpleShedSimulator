febc43e0505d34bf0f5d56ab92a8a36649a3370b
Egdus <anders524@hotmail.com>
2014-11-10 10:28:22 +0100

    Update test_simulation.py
    Updated with path

--- simpleshedsimulator/tests/test_simulation.py
+++ simpleshedsimulator/tests/test_simulation.py
@@ -1,6 +1,7 @@
-from simpleshedsimulator.core.act import activity
-from simpleshedsimulator.core.net import network
+import path
 import unittest
+from act import activity
+from net import network
 
 
 class TestActClass(unittest.TestCase):
@@ -11,28 +12,10 @@ class TestActClass(unittest.TestCase):
 		[activities[i].AssignSuccsesors(i+1) for i in range(10)]
 		[activities[i].AssignDuration(0) for i in range(10)]
 		[activities[i].AssignSuccsesors(i+1) for i in range(9)]
-		
-		
 		P = Network()
 		P.AddActivity(*activities)
-		#P.PlotGantt()
 		
 
-
-
-
-
-
 if __name__ == '__main__':
-	#unittest.main()
-	activities = [activity() for number in range(10)]
-	[activities[i].AssignID(i) for i in range(10)]
-	[activities[i].AssignDuration(10) for i in range(10)]
-	[activities[i].AssignSuccsesors(i+1) for i in range(9)]
+	unittest.main()
 	
-	
-	P = network()
-	P.AddActivity(*activities)
-	for i in P:
-		print i.GetStart()
-	P.PlotGantt()
